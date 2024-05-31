ca3a4d7f50ad0f923b47cfd1124cc71bdcfde956
trajano <matheus.trajano48@hotmail.com>
2024-05-30 22:46:00 -0300

    Fixed error in config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const apiSpaceID = import.meta.env.VITE_REACT_APP_CONTENTFUL_SPACE_ID;
 const apiAcessToken = import.meta.env.VITE_REACT_APP_CONTENTFUL_ACCESS_TOKEN;
-const basename = process.env.NODE_ENV === "production" ? basename = "/blog_reactjs" : "/";
+const basename = process.env.NODE_ENV === "production" ? "/blog_reactjs" : "/";
   
 export { apiSpaceID, apiAcessToken, basename };
